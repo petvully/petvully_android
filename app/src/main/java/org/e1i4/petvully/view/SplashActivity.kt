1d4e89aabad83303ba9958b9b3649c73556650e4
hyuna <akxmcse@gmail.com>
2023-06-29 01:12:02 +0900

    [feat] 화면 생성

---
+++ app/src/main/java/org/e1i4/petvully/view/SplashActivity.kt
@@ -0,0 +1,12 @@
+package org.e1i4.petvully.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.e1i4.petvully.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
