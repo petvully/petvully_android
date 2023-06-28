657e2c923381917ee3dcafa0453d651838bbd2df
WorldOneTop <dlwpdlf147@naver.com>
2023-06-29 05:12:03 +0900

    [feat] bottom nav bar setting

--- app/src/main/java/org/e1i4/petvully/view/MainActivity.kt
+++ app/src/main/java/org/e1i4/petvully/view/MainActivity.kt
@@ -1,9 +1,56 @@
 package org.e1i4.petvully.view
 
+import android.os.Bundle
+import androidx.fragment.app.Fragment
 import dagger.hilt.android.AndroidEntryPoint
+import org.e1i4.petvully.R
 import org.e1i4.petvully.base.BaseActivity
 import org.e1i4.petvully.databinding.ActivityMainBinding
+import org.e1i4.petvully.view.adoption.AdoptionFragment
+import org.e1i4.petvully.view.donation.DonationFragment
+import org.e1i4.petvully.view.home.HomeFragment
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
+    val homeFragment = HomeFragment()
+    val adoptionFragment = AdoptionFragment()
+    val donationFragment = DonationFragment()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        initFragments()
+        setNavigation()
+    }
+
+    private fun initFragments(){
+        supportFragmentManager.beginTransaction().apply {
+            add(R.id.main_fragment_container, homeFragment)
+            add(R.id.main_fragment_container, adoptionFragment)
+            add(R.id.main_fragment_container, donationFragment)
+            commit()
+        }
+        replaceFragment(homeFragment)
+    }
+    private fun setNavigation(){
+        binding.bottomNavigation.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.menu_main_bottom_home -> replaceFragment(homeFragment)
+                R.id.menu_main_bottom_adoption -> replaceFragment(adoptionFragment)
+                R.id.menu_main_bottom_donation -> replaceFragment(donationFragment)
+                else -> return@setOnItemSelectedListener false
+            }
+            return@setOnItemSelectedListener true
+        }
+    }
+    private fun replaceFragment(fragment:Fragment){
+        supportFragmentManager.beginTransaction().apply {
+            hide(homeFragment)
+            hide(adoptionFragment)
+            hide(donationFragment)
+            show(fragment)
+            commit()
+        }
+    }
+
 }
