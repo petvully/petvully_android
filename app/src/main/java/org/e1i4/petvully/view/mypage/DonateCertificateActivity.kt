7e59d1e6e4acb9e692c840bec203299e39fea173
hyuna <akxmcse@gmail.com>
2023-06-30 04:01:11 +0900

    [feat] 기부내역2 화면

--- app/src/main/java/org/e1i4/petvully/view/mypage/DonateCertificateActivity.kt
+++ app/src/main/java/org/e1i4/petvully/view/mypage/DonateCertificateActivity.kt
@@ -1,24 +1,23 @@
 package org.e1i4.petvully.view.mypage
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import org.e1i4.petvully.R
 import org.e1i4.petvully.base.BaseActivity
-import org.e1i4.petvully.databinding.ActivityCoinBinding
 import org.e1i4.petvully.databinding.ActivityDonateBinding
+import org.e1i4.petvully.databinding.ActivityDonateCertificateBinding
 import org.e1i4.petvully.view.SettingActivity
 import org.e1i4.petvully.view.message.MessageActivity
 
-class DonateActivity : BaseActivity<ActivityDonateBinding>(ActivityDonateBinding::inflate) {
+
+class DonateCertificateActivity : BaseActivity<ActivityDonateCertificateBinding>(ActivityDonateCertificateBinding::inflate) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setIntent()
     }
 
-    private fun setIntent(){
-        binding.icBack.setOnClickListener{
+    private fun setIntent() {
+        binding.icBack.setOnClickListener {
             finish()
         }
 
@@ -28,10 +27,6 @@ class DonateActivity : BaseActivity<ActivityDonateBinding>(ActivityDonateBinding
 
         binding.icMsg.setOnClickListener {
             startActivity(Intent(this, MessageActivity::class.java))
-        }
-
-        binding.ivBtn.setOnClickListener {
-            startActivity(Intent(this, DonateCertificateActivity::class.java))
         }
     }
 }
