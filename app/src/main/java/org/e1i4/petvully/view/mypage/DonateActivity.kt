63025bbf069a72a80a9a592b74f26cc4f9c116e5
hyuna <akxmcse@gmail.com>
2023-06-30 03:55:26 +0900

    [feat] 기부내역 화면

--- app/src/main/java/org/e1i4/petvully/view/mypage/DonateActivity.kt
+++ app/src/main/java/org/e1i4/petvully/view/mypage/DonateActivity.kt
@@ -1,12 +1,33 @@
 package org.e1i4.petvully.view.mypage
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import org.e1i4.petvully.R
+import org.e1i4.petvully.base.BaseActivity
+import org.e1i4.petvully.databinding.ActivityCoinBinding
+import org.e1i4.petvully.databinding.ActivityDonateBinding
+import org.e1i4.petvully.view.SettingActivity
+import org.e1i4.petvully.view.message.MessageActivity
 
-class DonateActivity : AppCompatActivity() {
+class DonateActivity : BaseActivity<ActivityDonateBinding>(ActivityDonateBinding::inflate) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_donate)
+
+        setIntent()
+    }
+
+    private fun setIntent(){
+        binding.icBack.setOnClickListener{
+            finish()
+        }
+
+        binding.icMenu.setOnClickListener {
+            startActivity(Intent(this, SettingActivity::class.java))
+        }
+
+        binding.icMsg.setOnClickListener {
+            startActivity(Intent(this, MessageActivity::class.java))
+        }
     }
 }
